c19d3759f3f9b4966d2b135cf0080e8b8c16bd20
Sam Gammon <sam@bloombox.io>
2018-10-13 14:43:40 -0700

    Update Swift package config, ignore resolution file

--- Package.swift
+++ Package.swift
@@ -11,13 +11,16 @@ let package = Package(
             targets: ["Client"])],
 
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", .branch("master")),
-        .package(url: "https://github.com/bloombox/grpc-swift.git", .branch("master"))],
+        .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMinor(from: "1.1.2")),
+        .package(url: "https://github.com/bloombox/grpc-swift.git", .branch("update-0-6-0"))],
 
     targets: [
         .target(
             name: "Client",
-            dependencies: ["Schema"]),
+            dependencies: ["Schema", "SwiftGRPC"]),
         .target(
             name: "Schema",
-            dependencies: ["SwiftGRPC"])])
+            dependencies: ["SwiftProtobuf", "SwiftGRPC"]),
+        .testTarget(name: "SchemaTests", dependencies: ["Schema"]),
+        .testTarget(name: "ClientTests", dependencies: ["Client"])])
+
