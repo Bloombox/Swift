33995b168763caa32707b8341364d5e6edbbdbea
Sam Gammon <sam@bloombox.io>
2018-10-14 18:20:38 -0700

    Add small example project to test pods

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Sam Gammon on 10/14/18.
+//  Copyright © 2018 Sam Gammon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
