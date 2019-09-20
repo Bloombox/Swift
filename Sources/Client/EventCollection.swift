73b2ebb05cd543e6586944084ec74408e62717e3
Sam Gammon <sam@bloombox.io>
2019-09-20 15:59:24 -0700

    Extend EventCollection with support for search/commercial events

--- Sources/Client/EventCollection.swift
+++ Sources/Client/EventCollection.swift
@@ -32,6 +32,10 @@ public enum EventCollection {
   /// to user data, and so on.
   case identity(IdentityEvent)
 
+  /// Specifies a "search event," which includes submitting a search and receiving results, and selecting a result from
+  /// that set of results, as applicable.
+  case search(SearchEvent)
+
   fileprivate static func encodeCollectionName(_ name: String) -> String? {
     return name.data(using: .utf8)?.base64EncodedString()
   }
@@ -46,13 +50,17 @@ public enum EventCollection {
         break
 
       case .commercial(let commercialEvent):
-        collection.name = EventCollection.encodeCollectionName(commercialEvent.label) ?? commercialEvent.label
+        collection.name = EventCollection.encodeCollectionName(commercialEvent.name!) ?? commercialEvent.name!
         collection.type = .commercial
         break
 
       case .identity(let identityEvent):
         collection.name = EventCollection.encodeCollectionName(identityEvent.label) ?? identityEvent.label
         collection.type = .identity
+
+      case .search(let searchEvent):
+        collection.name = EventCollection.encodeCollectionName(searchEvent.label) ?? searchEvent.label
+        collection.type = .experience
       }
     }
   }
@@ -63,25 +71,65 @@ public enum EventCollection {
 /// Enumerates types of commercial events. Used to specify a commercial event collection with a known event type.
 /// Commercial events are eligible for inclusion in marketing funnels, and specify specific purchase-flow events (i.e.
 /// impressions, views, conversions/actions, and so on).
-public enum CommercialEvent {
+public enum CommercialEvent: Labelled, Named {
+  /// Enumerates the subject item types for a given commercial event. These are defined by API support, and some
+  /// combinations may not be supported (i.e. order impressions and views aren't logical types).
+  public enum CommercialEventSubject: Labelled, Named {
+    /// The subject of the commercial event is a section of products.
+    case section
+
+    /// The subject of the commercial event is an individual product.
+    case product
+
+    /// The subject of the commercial event is a retail/commercial order.
+    case order
+
+    /// Label to use for this commercial event subject.
+    public var label: String? {
+      switch self {
+      case .section: return "Section"
+      case .product: return "Product"
+      case .order: return "Order"
+      }
+    }
+
+    /// Name to use for this commercial event subject.
+    public var name: String? {
+      switch self {
+      case .section: return "SECTION"
+      case .product: return "PRODUCT"
+      case .order: return "ORDER"
+      }
+    }
+  }
+
   /// Specifies a product or section impression, where a product item or product section was shown to the user, or
   /// otherwise presented as a selectable option.
-  case impression
+  case impression(CommercialEventSubject)
 
   /// Specifies a product or section view, where a user clicked or tapped on a product or product section, from a set of
   /// like-items displayed in a master-style section.
-  case view
+  case view(CommercialEventSubject)
 
   /// Specifies an action taken in an application, which may be a conversion event, or some other product action.
   /// Actions include zooming on photos, modifying filters, adding or removing items from the cart or bag, and so on.
-  case action
+  case action(CommercialEventSubject)
 
   /// Label to use for this commercial event type.
-  var label: String {
+  public var label: String? {
     switch self {
-    case .impression: return "IMPRESSION"
-    case .view: return "VIEW"
-    case .action: return "ACTION"
+    case .impression(let subject): return "Impression (\(subject.label!))"
+    case .view(let subject): return "View (\(subject.label!))"
+    case .action(let subject): return "Action (\(subject.label!))"
+    }
+  }
+
+  /// Name to use for this commercial event type.
+  public var name: String? {
+    switch self {
+    case .impression(let subject): return "IMPRESSION::\(subject.name!)"
+    case .view(let subject): return "VIEW::\(subject.name!)"
+    case .action(let subject): return "ACTION::\(subject.name!)"
     }
   }
 }
@@ -99,6 +147,25 @@ public enum IdentityEvent {
   var label: String {
     switch self {
     case .action: return "ACTION"
+    }
+  }
+}
+
+
+/// Enumerates types of search events. Used to specify an event where a user searched a catalog and received a
+/// resultset, or selected an item from that resultset.
+public enum SearchEvent {
+  /// Search query was submitted by a user, and a result-set was received of some kind.
+  case query
+
+  /// Result was selected by a user from a search query result-set.
+  case result
+
+  /// Label to use for this search event type.
+  var label: String {
+    switch self {
+    case .query: return "QUERY"
+    case .result: return "RESULT"
     }
   }
 }
